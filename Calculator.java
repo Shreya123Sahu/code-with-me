83777fc8a46da932f26a8304fbc8440b8de19156
ShreyaSahu <sahu123shreya@gmail.com>
2023-07-26 19:54:22 +0530

    first commit

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator {
+    public static void main(String[] args)
+    {
+        int a=10;
+        int b=5;
+        System.out.println("the sum of two no. is:" + (a+b));
+        System.out.println("the sub of two no. is:" + (a-b));
+        System.out.println("the mul of two no. is:" + (a*b));
+        System.out.println("the div of two no. is:" + (a/b));
+        System.out.println("the mod of two no. is:" + (a%b));
+    }
+    
+}
